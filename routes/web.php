84dbf845e6346de188d98aec77a5a2ebe50ee6d4
Guillermo Colotti <guille.theoffspring@gmail.com>
2019-03-28 15:33:48 -0300

    Added auth, models, migrations, controllers and routes for basic CRUDs.

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::middleware(['web', 'auth'])->group(function(){
+
+    Route::view('/', 'HomeController@index')->name('root');
+
+    Route::get('/home', 'HomeController@index')->name('home');
+
+    Route::resource('post', 'PostsController');
+
+    Route::resource('subjet', 'SubjetsController');
+
+    Route::resource('institute', 'InstitutesController');
+
+    Route::resource('user', 'UsersController');
+
 });
+
+Auth::routes();
